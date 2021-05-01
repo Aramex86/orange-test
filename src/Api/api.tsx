b89a71a8bb719431fd186bfd0c41f497be8bdc55
Aramex86 <slavbez@mail.ru>
2021-05-01 14:23:30 +0300

    show api

--- src/Api/api.tsx
+++ src/Api/api.tsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 
+const Api_Key= 'AIzaSyBhr_PmeQMzMCoERPzs3cfNNzuJ89Ld_Ss'
+
 export const booksApi = {
   getBooks() {
     return axios
       .get(
-        `https://www.googleapis.com/books/v1/volumes?q=mama&maxResults=8&key=${process.env.REACT_APP_BOOKS_API_KEY}`
+        `https://www.googleapis.com/books/v1/volumes?q=mama&maxResults=8&key=${Api_Key}`
       )
       .then((res) => {
         return res.data.items;
